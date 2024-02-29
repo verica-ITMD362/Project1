210d2da40bb25425578b523010b21cd068cdb467
verica-kar <vericakaranakova@gmail.com>
2024-02-29 11:01:22 -0600

    add js code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@ jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
   });
+
+// $(document).ready(function(){
+//   $('#input-terms').click(function () {
+//       //check if checkbox is checked
+//       if ($(this).is(':checked')) {
+            
+//           $('#submit').removeAttr('disabled'); //enable input
+            
+//       } else {
+//           $('#submit').attr('disabled', true); //disable input
+//           alert("Must agree to terms and conditions to submit");
+//       }
+//   });
+//   });
+
+function validate() {
+  if (document.getElementById('terms').checked) {
+      $('#submit').attr('disabled', false);
+  } else {
+    alert("You didn't check it! Let me check it for you.");
+    $('#submit').attr('disabled', true);
+  }
+}
+
+validate();
